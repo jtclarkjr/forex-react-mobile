c06091d3b6daff150ffb783d79c5575a98bc05c0
J <jtclarkjr@ucla.edu>
2025-09-06 12:41:33 +0900

    style: comment about expo ui

--- styles/index.ts
+++ styles/index.ts
@@ -7,3 +7,7 @@ export { createMainScreenStyles } from './mainScreen'
 export { createWatchlistItemStyles } from './watchlistItem'
 export { createPairDetailsStyles } from './pairDetails'
 export { createSwipeableWatchlistItemStyles } from './swipeableWatchlistItem'
+
+// https://docs.expo.dev/versions/v54.0.0/sdk/ui/
+// Has built in UI component library in expo 54 -  Expo UI
+// Worth checking out in future for prebuilt components and styles
